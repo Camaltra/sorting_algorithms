6bd5117dba8a1e925d7a97ddf064e209c469323e
Camaltra <mickael.boillaud@gmail.com>
2022-02-13 17:19:32 +0000

    Betty like it

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,35 +12,46 @@
 */
 void swap(int *array, int idx1, int idx2)
 {
-    int tmp;
+	int tmp;
 
-    tmp = array[idx1];
-    array[idx1] = array[idx2];
-    array[idx2] = tmp;
+	tmp = array[idx1];
+	array[idx1] = array[idx2];
+	array[idx2] = tmp;
 }
 
+/**
+* shell_sort - Sort an array using shell algorithm
+*
+* @array: Array to sort
+* @size: Size of the array
+*
+* Return: Anything, cause void function
+*/
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i;
-    int j, k;
+	size_t gap = 1, i;
+	int j, k;
 
-    while (gap <= size / 3)
-        gap = gap * 3 + 1;
+	if (!array || size <= 1)
+		return;
 
-    while (gap > 0)
-    {
+	while (gap <= size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
 		for (i = gap; i < size; i++)
 		{
 			k = array[i];
-            j = i - gap;
+			j = i - gap;
 			while (j >= 0 && k < array[j])
-            {
+			{
 				array[j + gap] = array[j];
-                j -= gap;
-            }
+				j -= gap;
+			}
 			array[j + gap] = k;
 		}
 		print_array(array, size);
 		gap = gap / 3;
-    }
+	}
 }
